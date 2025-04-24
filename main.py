3a3b8d8de2c0d5b2a7843beeb1120d8fea4cb817
Dominik Fay <dominik.fay@gmx.de>
2025-04-24 22:41:47 +0200

    Clean up code

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ import itertools as it
 import json
 import random
 from collections import defaultdict
+from typing import Optional
 
 import numpy as np
 import scipy.stats
 from tqdm import tqdm
 
 
+Clue = list['Predicate']
+WordLists = dict[str, list[str]]
+
+
 class Predicate(abc.ABC):
     @abc.abstractmethod
     def satisfies(self, word: str) -> bool:
-        raise NotImplemented
+        raise NotImplementedError
 
     @abc.abstractmethod
     def emoji(self, dark_mode=True):
-        raise NotImplemented
+        raise NotImplementedError
 
 
 class GreenMatch(Predicate):
@@ -55,7 +60,7 @@ class BlackMatch(Predicate):
         return "⬛" if dark_mode else "⬜"
 
 
-def make_predicates(guess: str, solution: str) -> [Predicate]:
+def make_predicates(guess: str, solution: str) -> list[Predicate]:
     preds = []
     for idx, (guess_letter, solution_letter) in enumerate(zip(guess, solution)):
         if guess_letter == solution_letter:
@@ -68,7 +73,20 @@ def make_predicates(guess: str, solution: str) -> [Predicate]:
     return preds
 
 
-def make_response_codes(guess: str, solution: str) -> np.ndarray:
+def make_response_code(guess: str, solution: str) -> np.uint8:
+    """Convert a clue to an unsigned 8-bit integer.
+    The clue is the response to a guess for a given solution.
+    Each color in the clue is represented by a digit:
+    - 0: black
+    - 1: yellow
+    - 2: green
+
+    The 8-bit integer is then obtained by converting the clue to the five corresponding numbers
+    and interpreting them as a base-3 number in Little Endian order.
+
+    For example, the clue "🟩⬛🟨⬛⬛" would be represented as 00102 in base-3, which is
+    2 * 3^0 + 0 * 3^1 + 1 * 3^2 + 0 * 3^3 + 0 * 3^4 = 2 + 0 + 9 + 0 + 0 = 11 in decimal.
+    """
     preds = np.zeros(5, dtype=np.uint8)
     for idx, (guess_letter, solution_letter) in enumerate(zip(guess, solution)):
         if guess_letter == solution_letter:
@@ -82,7 +100,8 @@ def make_response_codes(guess: str, solution: str) -> np.ndarray:
     return np.sum(preds * 3**np.arange(5), dtype=np.uint8)
 
 
-def convert_code(code: np.uint8, guess: str) -> [Predicate]:
+def convert_code(code: np.uint8, guess: str) -> list[Predicate]:
+    """Recover the list of predicates from the 8-bit response code (see make_response_code)."""
     code = (code // 3**np.arange(5)) % 3  # convert int to base-3 representation
     preds = []
     for idx, (c, letter) in enumerate(zip(code, guess)):
@@ -100,21 +119,21 @@ def convert_code(code: np.uint8, guess: str) -> [Predicate]:
 
 class Policy(abc.ABC):
     @abc.abstractmethod
-    def observe(self, guess: str, predicates: [Predicate]):
-        raise NotImplemented
+    def observe(self, guess: str, predicates: list[Predicate]):
+        raise NotImplementedError
 
     @abc.abstractmethod
     def guess(self) -> str:
-        raise NotImplemented
+        raise NotImplementedError
 
 
 class RandomValidPolicy(Policy):
-    def __init__(self, word_lists: {str: [str]}, list_='solutions'):
+    def __init__(self, word_lists: dict[str, list[str]], list_='solutions'):
         self.word_lists = word_lists
         self.predicates = []
         self.list_ = list_
 
-    def observe(self, guess: str, predicates: [Predicate]):
+    def observe(self, guess: str, predicates: Clue):
         self.predicates += predicates
 
     def guess(self) -> str:
@@ -123,14 +142,24 @@ class RandomValidPolicy(Policy):
 
 
 class GreedyHeuristicPolicy(Policy):
-    def __init__(self, word_lists: {str: [str]}, clue_matrix: np.ndarray, hard_mode=False, n_attempts=6):
+    """Guesses the word that minimizes the expected uncertainty about the solution, looking one step ahead.
+    
+    Formally, let $C(G, S)$ be the clue obtained by guessing $G$ when the solution is $S$.
+    We represent a clue by the list of words that are incompatible with the guess.
+    Then, for a sequence of clues $C_1, C_2, \\ldots, C_n$, the remaining uncertinty is given by
+    $U(C_1, C_2, \\ldots, C_n) = U_0 - |\\Cup_i C_i|$.
+
+    The greedy heuristic policy guesses the word $G$ that maximizes the expected uncertainty
+    $E[U(C_1, C_2, \\ldots, C_n, C(G_{n+1}, S))]$ over $G_{n+1}$, where the randomness is over the possible solutions $S$.
+    """
+    def __init__(self, word_lists: WordLists, clue_matrix: np.ndarray, hard_mode=False, n_attempts=6):
         self.word_lists = word_lists
         self.clue_matrix = clue_matrix
-        self.predicates = []
+        self.predicates: list[Predicate] = []  # flat list of predicates
         self.hard_mode = hard_mode
         self.remaining = n_attempts
 
-    def observe(self, guess: str, predicates: [Predicate]):
+    def observe(self, guess: str, predicates: Clue):
         self.predicates += predicates
         self.remaining -= 1
 
@@ -156,37 +185,8 @@ class GreedyHeuristicPolicy(Policy):
         return guess_list[action_idx]
 
 
-# Too inefficient; probably broken too
-class GreedyPolicy(Policy):
-    def __init__(self, word_lists: {str: [str]}, clue_matrix: np.ndarray, statistic=np.mean):
-        self.word_lists = word_lists
-        self.clue_matrix = clue_matrix
-        self.predicates = []
-        self.statistic = statistic
-
-    def observe(self, guess: str, predicates: [Predicate]):
-        self.predicates += predicates
-
-    def guess(self) -> str:
-        guess_list = self.word_lists['guesses']
-        solution_list = self.word_lists['solutions']
-        # possible_solutions = [w for w in self.word_lists['solutions'] if all(p.satisfies(w) for p in self.predicates)]
-        possible = [all(p.satisfies(w) for p in self.predicates) for w in solution_list]
-        remaining_matrix = np.zeros_like(self.clue_matrix, dtype=np.uint64)
-        for i, j in it.product(range(len(guess_list)), range(len(solution_list))):
-            # clue = self.clue_matrix[guess_list[i]][possible_solutions[j]]
-            clue_code = self.clue_matrix[i, j]
-            guess = guess_list[i]
-            clue = convert_code(clue_code, guess)
-            remaining_words = [all(p.satisfies(w) for p in clue) and possible[wi] for (wi, w) in enumerate(solution_list)]
-            remaining_matrix[i, j] = len(remaining_words)
-        remaining_list = self.statistic(remaining_matrix, axis=1)
-        action_idx = remaining_list.argmin()
-        return guess_list[action_idx]
-
-
 class InteractivePolicy(Policy):
-    def __init__(self, word_lists: {str: [str]}):
+    def __init__(self, word_lists: WordLists):
         self.word_lists = word_lists
 
     def guess(self) -> str:
@@ -196,23 +196,23 @@ class InteractivePolicy(Policy):
             s = input()
         return s
 
-    def observe(self, guess: str, predicates: [Predicate]):
+    def observe(self, guess: str, predicates: Clue):
         s = "".join(p.emoji() for p in predicates)
         print(s)
 
 
 class Game:
-    def __init__(self, word_lists: {str: [str]}, solution=None, hard_mode=False, n_attempts=6, policy=None):
+    def __init__(self, word_lists: WordLists, solution=None, hard_mode=False, n_attempts=6, policy=None):
         self.word_lists = word_lists
         self.solution = solution or random.choice(self.word_lists['solutions'])
         self.hard_mode = hard_mode
         self.policy = policy or RandomValidPolicy(word_lists)
         self.n_attempts = n_attempts
 
-    def play(self):
+    def play(self) -> tuple[list[str], list[Clue]]:
         guesses = []
         clues = []
-        for t in range(self.n_attempts):
+        for _t in range(self.n_attempts):
             guess = self.policy.guess()
             violates_hard_mode = self.hard_mode and not all(all(p.satisfies(guess) for p in clue) for clue in clues)
             if guess not in self.word_lists['guesses'] or violates_hard_mode:
@@ -226,7 +226,7 @@ class Game:
         return guesses, clues
 
 
-def new_policy(policy_name: str, word_lists, hard_mode, clue_matrix=None) -> Policy:
+def new_policy(policy_name: str, word_lists: WordLists, hard_mode: bool, clue_matrix: Optional[np.ndarray] = None) -> Policy:
     if policy_name == "interactive":
         policy = InteractivePolicy(word_lists)
     elif policy_name == "randomValid":
@@ -248,9 +248,10 @@ def main():
     args = parser.parse_args()
 
     print("Loading word lists...")
-    with open('words.json', 'rb') as fp:
+    with open('words.json', 'r', encoding='utf-8') as fp:
         word_lists = json.load(fp)
         word_lists['guesses'] += word_lists['solutions']
+        # TODO: why this filtering?
         word_lists['solutions'] = [w for w in word_lists['solutions'] if all(w[i] not in w[i + 1:] for i in range(4))]
 
     try:
@@ -260,7 +261,7 @@ def main():
         clue_matrix = np.zeros((len(word_lists['guesses']), len(word_lists['solutions'])), dtype=np.uint8)
         iterator = it.product(enumerate(word_lists['guesses']), enumerate(word_lists['solutions']))
         for (i, a), (j, w) in tqdm(iterator, total=len(word_lists['guesses']) * len(word_lists['solutions'])):
-            clue_matrix[i, j] = make_response_codes(a, w)
+            clue_matrix[i, j] = make_response_code(a, w)
         np.save('clue_matrix.npy', clue_matrix)
         print("Made clue matrix")
 
@@ -279,7 +280,7 @@ def main():
         else:
             solutions = random.sample(word_lists['solutions'], int(args.batch))
         results = {'statistics': defaultdict(int), 'games': []}
-        for r, solution in enumerate(tqdm(solutions)):
+        for _r, solution in enumerate(tqdm(solutions)):
             policy = new_policy(args.policy, word_lists, hard_mode=args.hard_mode, clue_matrix=clue_matrix)
             game = Game(word_lists, policy=policy, solution=solution, hard_mode=args.hard_mode)
             guesses, clues = game.play()
@@ -289,7 +290,7 @@ def main():
             else:
                 results['statistics'][-1] += 1
         print("Saving results")
-        with open('results.json', mode='w') as fp:
+        with open('results.json', mode='w', encoding='utf-8') as fp:
             json.dump(results, fp)
         print("Statistics:")
         stats = {k: round(v / sum(results['statistics'].values()), 3) for k, v in results['statistics'].items()}
